e3cbd116526a2e42bf2cbf937b50b896f889c896
IlliaShulha <102927025+IlliaShulha@users.noreply.github.com>
2022-10-07 03:35:28 -0400

    thursday night

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   integrations: {
-    __replace_me__: {
-      location: '@vue-storefront/__replace_me__-api/server',
+    wow: {
+      location: '@vue-storefront/wow-api/server',
       configuration: {}
     }
   }
